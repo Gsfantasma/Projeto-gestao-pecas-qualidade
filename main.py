1bf035abdff154a9b6038efe6a8258cff386bbf3
Glauco Adenauer <gsfantasma.dev@gmail.com>
2025-11-08 16:31:56 -0300

    Inclusão de estrutura de cadastro, correção de listagem e remoção de peças

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # projeto Gestão de Peças com Qualidade e Armazenamento
 
+#Fabrica de Apontadores GS
+
 import os
 
 from colorama import init, Fore
@@ -7,9 +9,13 @@ from colorama import init, Fore
 # Lista principal para armazenar as peças. Padronizado para minúsculas com underscore.
 cadastrar_pecas = [
     {
-        "Peça": "Mola",
-        "Data": "31/10/2025"
-    },
+        "ID_Peça": "Apontador-01",
+        "Cor": "Verde",
+        "Comprimento": "15",
+        "Peso": "100",
+        "Data_Fabricação": "31/10/2025",
+        "Status": " "
+    }
 ]
 
 init(autoreset=True)
@@ -26,18 +32,26 @@ def pausar():
 
 def adicionar_pecas():
     """Adiciona uma nova peça à lista cadastrar_pecas incluindo data, medidas e status."""
-    print(f"{Fore.CYAN}--- Adicionar Nova Peça ---")
-    novaPeca = input("Informe o nome da peça: ")
-    dataPeca = input("Informe a data de produção: ")
+    print(f"{Fore.CYAN}--- Adicionar Novo Apontador ---")
+    novaPeca = input("Informe o Id_Peça: ")
+    cor = input("Informe a Cor: ")
+    comp = input("Digite o comprimento: ")
+    peso = input("Digite o peso: ")
+    dataPeca = input("Informe a data de fabricação: ")
+    status = "Novo"
 
     # Estrutura (Dicionário)
-    nova_tarefa = {
-        "Peça": novaPeca,
-        "Data": dataPeca
+    novo_cadastro = {
+        "ID_Peça": novaPeca,
+        "Cor": cor,
+        "Comprimento": comp,
+        "Peso": peso,
+        "Data_Fabricação": dataPeca,
+        "Status": status
     }
     
     # Adiciona o dicionário à lista
-    cadastrar_pecas.append(nova_tarefa)
+    cadastrar_pecas.append(novo_cadastro)
     print (f"{Fore.MAGENTA}Peça adicionada com sucesso: {novaPeca}!")
 
 def exibir_pecas():
@@ -48,7 +62,7 @@ def exibir_pecas():
         
     print(f"{Fore.GREEN}--- Lista de Peças Cadastradas ---")
     for peca in cadastrar_pecas:
-        print (f"{Fore.GREEN}--> Peça: {peca['Peça']}, Data: {peca['Data']}")
+        print (f"{Fore.GREEN}--> Peça: {peca['ID_Peça']}, Cor: {peca['Cor']}, Comprimento: {peca['Comprimento']}cm, Peso: {peca['Peso']}g, Data_Fabricação: {peca['Data_Fabricação']}, Status: {peca['Status']}")
 
 def exibir_pecas_numeradas():
     """Exibe todas as peças numeradas para fins de remoção."""
@@ -59,7 +73,7 @@ def exibir_pecas_numeradas():
     print(f"{Fore.YELLOW}--- Peças para Remoção ---")
     for index, peca in enumerate(cadastrar_pecas):
         # Exibe o índice e os detalhes da peça
-        print (f"{Fore.YELLOW} [{index}] --> Peça: {peca['Peça']}, Data: {peca['Data']}")
+        print (f"{Fore.YELLOW} [{index}] --> Peça: {peca['ID_Peça']}, Cor: {peca['Cor']}, Comprimento: {peca['Comprimento']}cm, Peso: {peca['Peso']}g, Data_Fabricação: {peca['Data_Fabricação']}, Status: {peca['Status']}")
         
     return True # Retorna True se a lista foi exibida
 
@@ -74,7 +88,7 @@ def exibir_menu ():
     #print ("4 - Caixas Fechadas")
     #print ("5 - Relatorio Peças - Aprovadas")
     #print ("6 - Relatorio Peças - Reprovadas")
-    #print ("6 - Relatorio Peças - Reprovadas")
+    #print ("7 - Relatorio Geral")
     print ("0 - Sair")
     print (f"{Fore.BLUE}###########################")
 
@@ -105,7 +119,7 @@ while resposta != "0":
                 # Trata o caso de índice fora do intervalo
                 if 0 <= numero_pecas < len(cadastrar_pecas):
                     peca_removida = cadastrar_pecas.pop(numero_pecas)
-                    print(f"{Fore.RED}Peça '{peca_removida['Peça']}' removida com sucesso!")
+                    print(f"{Fore.RED}Peça '{peca_removida['ID_Peça']}' removida com sucesso!")
                 else:
                     print(f"{Fore.RED}ERRO: Número de peça inválido. Tente novamente.")
                     
