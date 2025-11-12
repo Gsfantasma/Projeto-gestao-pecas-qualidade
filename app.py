b37f909ff97222a925143c927a4ca0cf00652fa5
Glauco Adenauer <gsfantasma.dev@gmail.com>
2025-11-11 21:34:09 -0300

    Criação do arquivo app e definição de opções

---
+++ app.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk 
+
+# Configuração aparência
+
+# Criação da janela principal de login
+
+# Criação dos campos
+
+# Criação das funções da funcionalidade
+
+#Iniciar a aplicação
